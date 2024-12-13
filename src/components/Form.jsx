70dc651aaaf0fdc3d7f188367fd7b43a5a15924c
Nilakshhh <dnilaksh05@gmail.com>
2024-12-13 11:48:17 +0530

    completed form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,10 @@ import Heading from './Heading';
 
 const Form = () => {
   const [formData, setFormData] = useState({
+    name: '',
+    email: '',
     subject: '',
-    body: '',
+    message: '',
   });
 
   const handleChange = (e) => {
@@ -15,54 +17,74 @@ const Form = () => {
     e.preventDefault();
     const mailtoLink = `mailto:dnilaksh05@gmail.com?subject=${encodeURIComponent(
       formData.subject
-    )}&body=${encodeURIComponent(formData.body)}`;
+    )}&body=${encodeURIComponent(
+      `Name: ${formData.name}\nEmail: ${formData.email}\n\nMessage:\n${formData.message}`
+    )}`;
     window.location.href = mailtoLink;
   };
 
   return (
     <>
-      <Heading text1="LEt's connect" className="mt-[60px] sm:mt-[120px] !text-center" />
-      <div className='w-[100%] p-[24px]'>
+      <Heading text1="Let's connect" className="mt-[60px] sm:mt-[120px] !text-center" />
+      <div className="w-[100%] p-[24px]">
         <form onSubmit={handleSubmit}>
-          <div className='flex flex-col sm:flex-row gap-[5%]'>
-            <div className='flex flex-col w-[100%] sm:w-[45%]'>
-              <p className='text-[#ffffff] font-lato text-[18px]'>Name:</p>
+          <div className="flex flex-col sm:flex-row gap-[5%]">
+            <div className="flex flex-col w-[100%] sm:w-[45%]">
+              <p className="text-[#ffffff] font-lato text-[18px]">Name:</p>
               <input
-                className='bg-[#423F3E] pl-[12px] h-[5vh] text-[16px] mb-[12px] rounded-lg mt-[12px]'
+                className="bg-[#423F3E] pl-[12px] h-[5vh] text-[16px] mb-[12px] rounded-lg mt-[12px]"
                 type="text"
-                name="subject"
-                placeholder='John Doe'
-                value={formData.subject}
+                name="name"
+                placeholder="John Doe"
+                value={formData.name}
                 onChange={handleChange}
                 required
               />
             </div>
-            <div className='flex flex-col w-[100%] sm:w-[45%]'>
-              <p className='text-[#ffffff] font-lato text-[18px]'>Email:</p>
+            <div className="flex flex-col w-[100%] sm:w-[45%]">
+              <p className="text-[#ffffff] font-lato text-[18px]">Email:</p>
               <input
-                className='bg-[#423F3E] pl-[12px] h-[5vh] text-[16px] mb-[12px] mt-[12px] rounded-lg'
-                type="text"
-                name="subject"
-                placeholder='johndoe@gmail.com'
-                value={formData.subject}
+                className="bg-[#423F3E] pl-[12px] h-[5vh] text-[16px] mb-[12px] mt-[12px] rounded-lg"
+                type="email"
+                name="email"
+                placeholder="johndoe@gmail.com"
+                value={formData.email}
                 onChange={handleChange}
                 required
               />
             </div>
           </div>
-            <div className='flex flex-col w-[100%] sm:w-[95%]'>
-              <p className='text-[#ffffff] font-lato text-[18px]'>Message:</p>
-              <input
-                className='bg-[#423F3E] pl-[12px] h-[10vh] text-[16px] mb-[12px] mt-[12px] rounded-lg'
-                type="text"
-                name="subject"
-                placeholder='What would you like to talk about?'
-                value={formData.subject}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          <button className="text-center w-[100%] text-[#ffffff]" type="submit">Send Email</button>
+          <div className="flex flex-col w-[100%] sm:w-[95%]">
+            <p className="text-[#ffffff] font-lato text-[18px]">Subject:</p>
+            <input
+              className="bg-[#423F3E] pl-[12px] h-[5vh] text-[16px] mb-[12px] mt-[12px] rounded-lg"
+              type="text"
+              name="subject"
+              placeholder="Subject of your email"
+              value={formData.subject}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="flex flex-col w-[100%] sm:w-[95%]">
+            <p className="text-[#ffffff] font-lato text-[18px]">Message:</p>
+            <textarea
+              className="bg-[#423F3E] pl-[12px] pt-[12px] h-[20vh] text-[16px] mb-[12px] mt-[12px] rounded-lg"
+              name="message"
+              placeholder="What would you like to talk about?"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="w-[100%] flex justify-center items-center">
+            <button
+              className="text-center w-[45%] sm:w-[15%] mt-[12px] h-[5vh] text-[18px] bg-[#E85C0D] rounded-lg text-[#ffffff]"
+              type="submit"
+            >
+              Send Email
+            </button>
+          </div>
         </form>
       </div>
     </>
