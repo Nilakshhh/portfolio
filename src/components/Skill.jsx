28831cc826142e75dfbd94b8e0eb122f0d8ff6fd
Nilakshhh <dnilaksh05@gmail.com>
2025-03-19 14:02:25 +0530

    working on skill, still need to complete

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -4,7 +4,7 @@ function Skill() {
     return(
     <div className="flex flex-col gap-[12px]">
         <div className="flex flex-row items-center gap-[12px] sm:gap-[0] justify-between w-[100%] mt-[60px]">
-            <SkillCompo img="/f.png" text="React.JS" />        
+            <SkillCompo img="/images.png" text="Full Stack Web Development" />        
             <SkillCompo img="/react.svg" text="React.JS" /> 
             <SkillCompo img="/react.svg" text="React.JS" /> 
         </div>        
--- src/components/SkillCompo.jsx
+++ src/components/SkillCompo.jsx
@@ -1,21 +1,10 @@
 function SkillCompo({img, text}) {
     return(
         <div className="w-[30%] h-[100%] flex flex-col sm:flex-row gap-[20px] items-center justify-center sm:justify-start sm:pl-[24px]">
-            <img src={img} alt="logo" className="sm:w-[60px] w-[48px]"></img>
+            <img src={img} alt="logo" className="sm:w-[60px] w-[60px]"></img>
             <p className="text-[#ffffff] text-[24px] sm:text-[18px] font-lato">{text}</p>
         </div>
     )
 }
 
 export default SkillCompo;
-
-// function SkillCompo({img, text}) {
-//     return(
-//         <div className="flex flex-col w-[30%] h-[96px] gap-[12px] justify-left items-center rounded-[24px]">
-//             <img src={img} alt="logo" className="w-[84px]"></img>
-//             <p className="text-[#ffffff] text-[24px] font-lato">{text}</p>
-//         </div>
-//     )
-// }
-
-// export default SkillCompo;
